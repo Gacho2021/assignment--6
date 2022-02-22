d21683736479a5b732a82d4891748e38e4ef0fd8
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-02-22 15:22:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Get first list item 
+var firstItem = document.getElementById('one');
 
+ // If it has class attribute
+if (firstItem.hasAttribute('class')) { 
+    
+    // Get the attribute
+  var attr = firstItem.getAttribute('class');  
+ 
+  // Add the value of the attribute after the list
+  var el = document.getElementById('scriptResults');
+  el.innerHTML = '<p>The first 3 item have a class name: ' + attr + '</p>';
+
+}
